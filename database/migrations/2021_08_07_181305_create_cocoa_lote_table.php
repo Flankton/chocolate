a17bd48d74ab26f8621d0c2a3b0e202e18d6e183
Flankton <will.engecomp@gmail.com>
2021-08-08 13:02:21 -0300

    feat: chocolate

---
+++ database/migrations/2021_08_07_181305_create_cocoa_lote_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCocoaLoteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cocoa_lote', function (Blueprint $table) {
+            $table->id();
+            $table->string('description')->nullable;
+            $table->unsignedBigInteger('provider_id');
+            $table->boolean('organic');
+            $table->timestamps();
+            $table->foreign('provider_id')->references('id')->on('provider');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cocoa_lote');
+    }
+}
